2f15dbaf712ebee3030af84477c3f099d36449cd
Christof Angermueller <cangermueller@gmail.com>
2014-10-05 18:34:02 +0100

    Minor changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,8 +16,8 @@
 import sys
 import os
 
-os.command('pip install numpy')
-os.command('pip install scipy')
+os.system('pip install numpy')
+os.system('pip install scipy')
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
